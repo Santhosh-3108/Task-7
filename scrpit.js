932e8ef78d5bcd10584c4ce2f2d86456ae94dadc
Santhosh-3108 <gokulloganathan45@gmail.com>
2025-04-13 21:24:23 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const ctaButton = document.querySelector(".cta");
+    ctaButton.addEventListener("click", () => {
+        alert("Thanks for signing up!");
+    });
+});
